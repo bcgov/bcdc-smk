a0ae70a76ead706019ba3eaeaf35254ef039b224
GuyLafleur <kevin.netherton@gov.bc.ca>
2020-09-25 12:00:33 -0700

    fixing smk defs in index.html * Working with Ben, modify slighly how SMK gets defined in the index.html

--- smk-init.js
+++ smk-init.js
@@ -1,6 +1,7 @@
 SMK.INIT( {
     containerSel: '#smk-map-frame',
-    config: [ './smk-config.json', '?' ]
+    config: [ './smk-config.json', '?smk-' ]
+    // ?smk- added so that we do not have to update the catalogue code
 } )
 .then( function ( smk ) {
     // SMK initialized
